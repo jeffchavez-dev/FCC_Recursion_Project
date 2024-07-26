dd8c55edaddd689c19a7d9c08d4ed75ba26694ca
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-26 19:19:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const convertBtn = document.getElementById('convert-btn')
 const result = document.getElementById('result')
 
 const checkUserInput = () => {
-    // console.log(numberInput.value)
+    console.log(numberInput.value)
 }
 
 convertBtn.addEventListener('click', checkUserInput)
 
 numberInput.addEventListener('keydown', (e) => {
     if (e.key == "Enter") {
-        
+        checkUserInput()
     }
 })
