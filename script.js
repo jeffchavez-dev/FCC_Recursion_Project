fd806b53295033c80b5d9a13d0a4e4a194fe18a6
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-26 19:04:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const numberInput = document.getElementById('number-input')
 const convertBtn = document.getElementById('convert-btn')
 const result = document.getElementById('result')
 
-function checkUserInput () {
+const checkUserInput = () => {
     console.log(numberInput.value)
 }
 
 convertBtn.addEventListener('click', () => {
-    checkUserInput()
+    checkUserInput
 })
