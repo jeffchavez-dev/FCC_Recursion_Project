05ebf3839c2aba1176e07c4cc48b9870b7bcea37
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-27 13:19:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -41,14 +41,5 @@ numberInput.addEventListener('keydown', (e) => {
 
 
 const decimalToBinary = (input) => {
-    let binary = "";
-    if (input == 0) {
-        binary = "0"
-    }
-    while (input > 0) {
-        binary = (input % 2) + binary
-        input = Math.floor(input / 2)
-    }
 
-    result.innerText = binary
 }
