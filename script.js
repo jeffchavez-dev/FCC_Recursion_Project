83545aa2e4b9b696043da46d50a022444634e046
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-27 12:54:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,31 +22,6 @@ numberInput.addEventListener('keydown', (e) => {
 
 
 const decimalToBinary = (input) => {
-    // const inputs = [];
-    // const quotients = [];
-    // const remainders = [];
-    
-    // if (input === 0) {
-    //     result.innerText = "0"
-    //     return
-    // }
-
-    // while (input > 0) {
-    //     const quotient = Math.floor(input / 2);
-    //     const remainder = input % 2
-    //     input = quotient;
-    //     inputs.push(input);
-    //     quotients.push(quotient);
-    //     remainders.push(remainder);
-    // }
-
-    // console.log("Inputs: ", inputs)
-    // console.log("Quotients: ", quotients)
-    // console.log("Remainders: ", remainders)
-
-
-    // result.innerText = remainders.reverse().join('')
-
     let binary = "";
     if (input == 0) {
         binary = "0"
