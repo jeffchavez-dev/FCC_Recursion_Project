20bdcd52cff36a67688ead0b020f4eab640d3a7c
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-29 17:12:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const showAnimation = () => {
     result.innerText = "Call Stack Animation"
     animationData.forEach(obj => {
         setTimeout(() => {
-            animationContainer.innerHTML += ``
+            animationContainer.innerHTML += `<p id="${obj.inputVal}"></p>`
         },obj.addElDelay)
     });
 }
