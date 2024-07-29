3806c3618867a32889dffc27bf71743e22a245c6
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-29 16:48:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ const result = document.getElementById('result')
 
 
 const decimalToBinary = (input) => {
-    if (input === 0) {
-        return "0";
-    }  else if (input === 1) {
-        return "1";
-    }   else {
+    if (input === 0 || input === 1) {
+        return String(input);
+    }  else {
         return decimalToBinary(Math.floor(input / 2)) + (input % 2)
     }
 }
